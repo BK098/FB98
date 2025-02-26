0c12c3d425c2f4a47bc412fb90393f7344956514
BK098 <buikhang122004@gmail.com>
2025-02-27 05:40:26 +0700

    Refactor product handlers and update validation
    - Added `Id` property to `GetAllComboResponse`.
    - Introduced `ICategoryRepository` in `CreateProductCommandHandler` and `UpdateProductCommandHandler`.
    - Updated constructors to accept `ICategoryRepository`.
    - Added category existence validation in product handlers.
    - Modified success responses in product handlers.
    - Refactored image handling in `UpdateProductCommandHandler`.
    - Removed `ProductImage` validation rule in `UpdateProductValidation`.

--- src/Modules/Catalog/FB98.Modules.Catalog.Application/ComboManagement/GetAll/GetAllComboResponse.cs
+++ src/Modules/Catalog/FB98.Modules.Catalog.Application/ComboManagement/GetAll/GetAllComboResponse.cs
@@ -4,6 +4,7 @@ namespace FB98.Modules.Catalog.Application.ComboManagement.GetAll
 {
 	public class GetAllComboResponse : IResponse
 	{
+		public Guid Id { get; set; }
 		public string? Name { get; set; }
 		public string? Description { get; set; }
 		public decimal? Price { get; set; }
